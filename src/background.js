fb36755fdaa4a1d9952da5e8a5347927f26b1564
Alexandre Macabies <web+oss@zopieux.com>
2020-11-29 01:29:43 +0100

    Use a new heuristic to detect buttons, because classes are so 2019.
    Spotify is doing Modern Frontend Development™, which means making
    everything more complicated than it needs to be because why not.
    
    I am not proud of this cursed code but I don't see better. ¯\_(ツ)_/¯

--- src/background.js
+++ src/background.js
@@ -1,15 +1,62 @@
 function sendCommandToTab(command, tab) {
-  let targets = {
-    'play-pause': ['play', 'pause'],
-    'next': ['skip-forward'],
-    'previous': ['skip-back'],
-    'shuffle': ['shuffle'],
-    'repeat': ['repeat', 'repeatonce'],
-    'like': ['heart', 'heart-active'],
-  }[command];
-  if (!targets) return;
-  const selector = targets.map(t => `.control-button.spoticon-${t}-16`).join(', ');
-  const code = `document.querySelector("${selector}").click();`;
+  // Some buttons have a convenient CSS class. Use that to find it and click.
+  function usingClass(command) {
+    const targets = {
+      'play-pause': ['play', 'pause'],
+      'next': ['skip-forward'],
+      'previous': ['skip-back'],
+      'shuffle': ['shuffle'],
+      'repeat': ['repeat', 'repeatonce'],
+      'like': ['heart', 'heart-active'],
+    }[command];
+    if (!targets) return null;
+    const sel = targets.map(t => `.control-button.spoticon-${t}-16`).join(', ');
+    return `
+      (function () {
+        const e = document.querySelector("${sel}");
+        if (!e) throw "";
+        e.click();
+      })`;
+  }
+
+  // A Very Cursed search for a specific iconography using CSS <path> d=
+  // attribute. Then go up the DOM tree to find the parent button and click.
+  function usingSvg(command) {
+    const paths = {
+      'previous': ['M13 2.5L5 7.119V3H3v10h2V8.881l8 4.619z'],
+      'play-pause': ['M0 0h16v16H0z', 'M4.018 14L14.41 8 4.018 2z'],
+    }[command];
+    if (!paths) return null;
+    const sel = paths.map(p => `.player-controls path[d="${p}"]`).join(', ');
+    return `
+      (function () {
+        let e = document.querySelector('${sel}');
+        if (!e) throw "";
+        while (e && e.tagName.toLowerCase() !== "button") e = e.parentNode;
+        if (!e) throw "";
+        e.click();
+      })`;
+  }
+
+  // Let's have multiple methods to find the right button, because Spotify
+  // decided front-end development was more exciting with randomized CSS
+  // classes... but not for all UI elements of course. Yay, consistency!
+  const codes = [usingClass, usingSvg].map(e => e.call(this, command)).filter(e => !!e);
+
+  // No method available for that command. Bail out.
+  if (!codes.length) {
+    console.error(`nothing to do for ${command}`);
+    return;
+  }
+
+  // Use some reduce() smartness to use each method one after the other.
+  // Each method shoud throw to communicate that it failed to click its button.
+  // Use 'return;' to stop at the first successful method.
+  const tryCascade = codes.reverse().reduce(
+    (acc, fun) => `try { ${fun}(); return; } catch (_) { ${acc} }`,
+    '');
+  // Wrap inside a function to allow for the early 'return;' above.
+  const code = `(function () { ${tryCascade} })();`;
   chrome.tabs.executeScript(tab.id, { code });
 }
 
