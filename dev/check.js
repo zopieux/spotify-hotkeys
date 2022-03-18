6e5c697c47be0ba44846522300a2257441db5893
Alexandre Macabies <web+oss@zopieux.com>
2022-03-18 22:11:55 +0100

    Set more lenient image diff.

--- dev/check.js
+++ dev/check.js
@@ -235,7 +235,6 @@ async function compareWithGolden() {
         expectedFilename: expected,
         diffFilename: `/tmp/diff-${name}`,
         generateOnlyDiffFile: true,
-        options: {threshold: 0.05, includeAA: true},
       });
       if (!diff.imagesAreSame) {
         hasDiff = true;
